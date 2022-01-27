c5104e18658afc9d5b607b6a6fe5e4b0e1216d93
JC Trilamanila <trilamanila@gmail.com>
2022-01-27 05:06:29 -0500

    Created Gameboard

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from "./setup";
+import GameBoard from "./GameBoard";
+
+//Dom Elements
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
+
+//Game Constants
+const POWER_PILL_TIME = 10000; //ms
+const GLOBAL_SPEED = 80; //ms
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+
+//Initail Setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {}
+
+function checkCollision(pacman, ghosts) {}
+
+function gameLoop(pacman, ghosts) {}
+
+function startGame() {}
